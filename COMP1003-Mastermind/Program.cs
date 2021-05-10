52f7a1155bc42b38e51f209acad0f7b76082246a
bc-carter <72526673+bc-carter@users.noreply.github.com>
2021-05-10 13:33:32 +0100

    full functionality added

--- COMP1003-Mastermind/Program.cs
+++ COMP1003-Mastermind/Program.cs
@@ -7,39 +7,45 @@ namespace COMP1003_Mastermind
         
         private bool active = true;
 
-        public enum GameStatus { NULL, STARTED, POSITIONSSELECTED, PLAYING, QUIT }; //stores state of game
-        private GameStatus action = GameStatus.NULL;
+        public enum GameStatus { NULL, STARTED, POSITIONSSELECTED, PLAYING, QUIT, END }; //stores state of game
+        private GameStatus action = GameStatus.NULL; // initially sets status to NULL
 
         private bool isPlaying = false;
 
-        string[] colours = { "Blue", "Yellow", "Red", "Orange", "Green", "Pink", "Purple", "Brown", "White" };
+        string[] colours = { "", "Blue", "Yellow", "Red", "Orange", "Green", "Pink", "Purple", "Brown", "White" }; // used to tell user colours - unnescessary as game solely uses numbers
 
-        int N = 0;
-        int M = 0;
+        int N = 0; // stores code length
+        int M = 0; // stores code range (colours)
 
+        int[,] queue = new int[150,9]; // initialises queue 2d array
+
+        int guesses = 0; // stores current guess number
 
         bool correctrange = false;
+        bool checkint = false;
 
-        private int[] currentCode;
+        private int[] currentCode; // stores secret code
 
 
-        private int[] GenerateRandomNumbers(int Size, int maxRange)
+        // RandomNumberGenerator generates a secret code based on N and M parameters
+        private int[] RandomNumberGenerator(int Size, int maxRange)
         {
             int eachNumber;
             int[] randomNumber = new int[Size];
             Random rnd = new Random();
-
             for (int i = 0; i < Size; i++)
             {
                 eachNumber = rnd.Next(1, maxRange);
                 randomNumber[i] = eachNumber;
 
-                //Console.Write(eachNumber); //used for testing
+                // Console.Write(eachNumber); //used for testing
             }
             Console.WriteLine();
             return randomNumber;
         }
 
+        // GameLoop is the core of the game, it determines what happens 
+        // next in the game depending on what the current GameStatus is
         public void GameLoop(bool active)
         {
 
@@ -47,10 +53,23 @@ namespace COMP1003_Mastermind
             {
                 if (action == GameStatus.STARTED)
                 {
-                    Console.WriteLine("How many positions would you like to play using?");
-                    string positions = Console.ReadLine();
-                    int.TryParse(positions, out N);
-                    action = GameStatus.POSITIONSSELECTED;
+                    while (checkint == false)
+                    {
+                        Console.WriteLine("How many positions would you like to play with?");
+                        string positions = Console.ReadLine();
+                        int.TryParse(positions, out N);
+                        if (N >= 1)
+                        {
+                            action = GameStatus.POSITIONSSELECTED;
+                            checkint = true;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Please enter a number equal to or greater than 1");
+                            Console.ResetColor();
+                        }
+                    }
                 }
                 if (action == GameStatus.POSITIONSSELECTED)
                 {
@@ -61,25 +80,27 @@ namespace COMP1003_Mastermind
                         int.TryParse(colours, out M);
                         if (M >= 1 && M <= 9)
                         {
-                            currentCode = GenerateRandomNumbers(N, M + 1);
+                            currentCode = RandomNumberGenerator(N, M + 1);
                             action = GameStatus.PLAYING;
                             correctrange = true;
                             Console.ForegroundColor = ConsoleColor.Black;
                             Console.BackgroundColor = ConsoleColor.White;
-                            Console.WriteLine(Environment.NewLine + "You have chosen to play with " + N + " pegs.");
+                            Console.WriteLine(Environment.NewLine + "You have chosen to play with " + N + " positions.");
                             Console.ResetColor();
                         }
                         else
                         {
+                            Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("Please enter a number in the range 1-9");
+                            Console.ResetColor();
                         }
                     }
                 }
                 while (action == GameStatus.PLAYING)
                 {
                     Console.WriteLine("Enter your guess");
-                    int[] userArray = GetUserGuess(N);
-                    int hits = CountHits(currentCode, userArray);
+                    int[] userCode = ProcessGuess(N);
+                    int hits = HitCalculator(currentCode, userCode);
                     if (hits == N)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
@@ -89,14 +110,28 @@ namespace COMP1003_Mastermind
                         {
                             Console.Write(currentCode[j] + " ");
                         }
-                        action = GameStatus.QUIT;
+                        Console.Write("- ");
+                        for (int j = 0; j < N; j++)
+                        {
+                            Console.Write(colours[currentCode[j]] + " ");
+                        }
+                        Console.ResetColor();
+                        action = GameStatus.END;
                     }
                 }
-
+                if (action == GameStatus.END) // replay feature
+                {
+                    guesses = 0;
+                    correctrange = false;
+                    checkint = false;
+                    Console.WriteLine(Environment.NewLine + "Would you like to play again? (yes/no)");
+                }
             }
         }
-
-        public void ProcessInput(string input)
+        
+        // ProcessInput is used to determine the action of the game
+        // based on the users input
+        public void ProcessInput(string input) 
         {
             input = input.ToLower();
             switch (input)
@@ -114,52 +149,100 @@ namespace COMP1003_Mastermind
             }
         }
 
-        public static int[] GetUserGuess(int userSize)
+        // ProcessGuess is used to ask the users' input, it also prints guess history
+        private int[] ProcessGuess(int userSize) // asks for users guess'
         {
             int number = 0;
             int[] userGuess = new int[userSize];
-            for (int i = 0; i < userGuess.Length; i++)
+            for (int i = 0; i < N; i++) // reads users guess'
             {
-                Console.Write("Digit {0}: ", (i + 1));
-                ConsoleKeyInfo info = Console.ReadKey();
-                char inputtedKey = info.KeyChar;
-                number = int.Parse(inputtedKey.ToString());
-                userGuess[i] = number;
-                Console.WriteLine();
-                //Console.Write(number);
+                try
+                {
+                    Console.Write("Digit {0}: ", (i + 1));
+                    ConsoleKeyInfo info = Console.ReadKey();
+                    char inputtedKey = info.KeyChar;
+                    number = int.Parse(inputtedKey.ToString());
+                    userGuess[i] = number;
+                    queue[guesses, i] = number;
+                    Console.WriteLine();
+                }
+                catch (System.FormatException) // if wrong format detected, throws error message
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Environment.NewLine + "Please enter a number in the range of 1 - {0}!", M);
+                    i--;
+                    Console.ResetColor();
+                }
             }
-            Console.WriteLine();
             Console.Clear();
-            Console.Write("Your guess: ");
-            for (int i = 0; i < userGuess.Length; i++)
-            {
-                Console.Write(userGuess[i] + " ");
-            }
+            Console.WriteLine("******************************************************"); // prints guess history
+            Console.Write("Guess History: ");
             Console.WriteLine();
-           
+            guesses++; // increment current guess number
+            for (int c = 0; c < guesses; c++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    Console.Write(queue[c,i] + " ");
+                }
+                Console.Write("- ");
+                for (int i = 0; i < N; i++)
+                {
+                    Console.Write(colours[queue[c,i]] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("******************************************************");
             return userGuess;
         }
-        public static int CountHits(int[] currentCode, int[] userArray)
+        
+        // HitCalculator is used to calculate hits based on the users previous guess
+        private int HitCalculator(int[] currentCode, int[] userCode) // calculates hits / misses based on users guess and the generated code
         {
-            int hit = 0;
-            int miss = 0;
-            int hits = 0;
+            int[] cloneCurrentCode = new int[N];
+            int[] cloneUserCode = new int[N];
 
-            for (int i = 0; i < currentCode.Length; i++)
+            int blackPeg = 0;
+            int miss;
+            int totalhits;
+            int whitePeg = 0;
+
+            Array.Copy(currentCode, cloneCurrentCode, N); // copy secret code to clone array
+            Array.Copy(userCode, cloneUserCode, N); // copy user guess to clone array
+
+            for (int a = 0; a < N; a++)
             {
-                if (currentCode[i] == userArray[i])
+                for (int b = 0; b < N; b++)
                 {
-                    hit ++;
-                    hits = hit;
-                }
-                else
-                {
-                    miss ++;
+                    if (cloneUserCode[a] == cloneCurrentCode[b]) // increments partial hits when conditions met (white pegs)
+                    {
+                        if (cloneUserCode[a] != 0)
+                        {
+                            whitePeg++;
+                            cloneUserCode[a] = 0;
+                            cloneCurrentCode[b] = 0;
+                        }
+                    }
                 }
             }
-            Console.WriteLine("Results: {0} Hit(s), {1} Miss(es)", hit, miss);
-            return hits;
+            for (int i = 0; i < N; i++)
+            {
+                if (userCode[i] == currentCode[i]) //increments exact hits when conditions met (black pegs)
+                {
+                    blackPeg++;
+                    whitePeg--;
+                }
+            }
+            totalhits = blackPeg + whitePeg;
+            miss = N - totalhits;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Results: {0} Black Peg(s), {1} White Peg(s), {2} Miss(es)", blackPeg, whitePeg, miss); // prints results
+            Console.ResetColor();
+            return blackPeg;
         }
+        
+        // main is used to call other methods when initially starting the game
         static void Main(string[] args)
         {
             Mastermind mind = new Mastermind();
@@ -170,7 +253,7 @@ namespace COMP1003_Mastermind
             Console.ResetColor();
             Console.WriteLine("Would you like to start a new game? (yes/no)");
 
-            // Loops through the input and determines when the game should quit
+            // loops through user input and determines when to quit game
 
             while (mind.active && mind.action != GameStatus.QUIT)
             {
@@ -178,12 +261,10 @@ namespace COMP1003_Mastermind
                 {
                     Console.WriteLine("Your Command: ");
                 }
-
                 input = Console.ReadLine();
                 Console.WriteLine(Environment.NewLine);
                 mind.ProcessInput(input);
                 mind.GameLoop(mind.active);
-
             }
         }
     }
